52a4ada272a10150c3d7a2b4cbab9232438d0865
Kazim Walji <@>
2021-05-21 20:09:32 -0500

    Initial Commit

---
+++ MusicViewer/MusicViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicViewerApp.swift
+//  MusicViewer
+//
+//  Created by Kazim Walji on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
